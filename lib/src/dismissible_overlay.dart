2ec1f1cb87de323992f5a2a1e8445bf1dfda5ba8
Lars <larsb24@outlook.com>
2021-02-13 18:16:15 +0100

    Close overlay when esc key is pressed

--- lib/src/dismissible_overlay.dart
+++ lib/src/dismissible_overlay.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:utopia_wm/src/dismissible_overlay_entry.dart';
 import 'package:utopia_wm/src/window_hierarchy.dart';
@@ -19,6 +20,7 @@ class DismissibleOverlay extends StatefulWidget {
 class _DismissibleOverlayState extends State<DismissibleOverlay>
     with SingleTickerProviderStateMixin {
   static final _childKey = GlobalKey();
+  final _focusNode = FocusNode(canRequestFocus: true);
 
   @override
   void initState() {
@@ -32,6 +34,7 @@ class _DismissibleOverlayState extends State<DismissibleOverlay>
       parent: widget.entry.animationController,
     );
     widget.entry.animationController.animateTo(1);
+    _focusNode.requestFocus();
 
     super.initState();
   }
@@ -52,32 +55,40 @@ class _DismissibleOverlayState extends State<DismissibleOverlay>
           key: _childKey,
         );
 
-        return Listener(
-          behavior: HitTestBehavior.translucent,
-          onPointerDown: (event) {
-            final RenderBox box =
-                _childKey.currentContext!.findRenderObject() as RenderBox;
-
-            final position = box.localToGlobal(Offset.zero);
-            final rect = position & box.size;
-            final hitTested =
-                box.hitTest(BoxHitTestResult(), position: event.position);
-            final pointInsideRect = rect.contains(event.position);
-
-            if (hitTested) return;
-            if (pointInsideRect) return;
-            _dismissOverlay();
+        return RawKeyboardListener(
+          focusNode: _focusNode,
+          onKey: (event) {
+            if (event.isKeyPressed(LogicalKeyboardKey.escape)) {
+              _dismissOverlay();
+            }
           },
-          child: Stack(
-            children: [
-              IgnorePointer(
-                child: SizedBox.expand(
-                  child:
-                      entry.background ?? Container(color: Colors.transparent),
+          child: Listener(
+            behavior: HitTestBehavior.translucent,
+            onPointerDown: (event) {
+              final RenderBox box =
+                  _childKey.currentContext!.findRenderObject() as RenderBox;
+
+              final position = box.localToGlobal(Offset.zero);
+              final rect = position & box.size;
+              final hitTested =
+                  box.hitTest(BoxHitTestResult(), position: event.position);
+              final pointInsideRect = rect.contains(event.position);
+
+              if (hitTested) return;
+              if (pointInsideRect) return;
+              _dismissOverlay();
+            },
+            child: Stack(
+              children: [
+                IgnorePointer(
+                  child: SizedBox.expand(
+                    child: entry.background ??
+                        Container(color: Colors.transparent),
+                  ),
                 ),
-              ),
-              child,
-            ],
+                child,
+              ],
+            ),
           ),
         );
       },
