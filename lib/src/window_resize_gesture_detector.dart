8077efdcd82f0191198bb43a1afccd74d42765cf
Lars <larsb24@outlook.com>
2021-03-16 19:09:59 +0100

    Fix import

--- lib/src/window_resize_gesture_detector.dart
+++ lib/src/window_resize_gesture_detector.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 class WindowResizeGestureDetector extends StatelessWidget {
   final double? borderThickness;
